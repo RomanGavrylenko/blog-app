dc9cd2bb4a2cce34888655e55f13c7180559c315
roma <Roman>
2020-01-22 21:28:29 +0300

    edit client part to ts

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,14 +3,11 @@ import logo from './logo.svg';
 import './App.css';
 import axios from 'axios';
 
-function App() {
 
-  useEffect( async () => {
-    // const create = await axios.post('/api/product', { name: 'first product2', description: 'first description2'})
+const App: React.FC = () => {
 
-    const data = await axios.get('/api/product')
-    console.log( data);
-    
+  useEffect( () => {
+     axios.get('/api/product').then(res => console.log(res))
   }, [])
 
   return (
@@ -18,7 +15,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>src/App.tsx</code> and save to reload.
         </p>
         <a
           className="App-link"
